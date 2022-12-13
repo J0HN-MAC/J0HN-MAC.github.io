f42d88073d43db676dfa77d51e0afcc05c5a9f3b
J0HN-MAC <johnmac1827@gmail.com>
2022-12-12 23:46:01 -0500

    updated charts.js after deliverable 3

--- charts.js
+++ charts.js
@@ -60,61 +60,84 @@ function buildCharts(sample) {
   d3.json("samples.json").then((data) => {
     console.log("data" , data);
 
-    // Deliverable 1: 3. Create a letiable that holds the samples array. 
+    // Deliverable 1: 3. Create a variable that holds the samples array. 
     let samples = data.samples;
     // console.log("metadata" , metadata)
 
-    // Deliverable 1: 4. Create a letiable that filters the samples for the object with the desired sample number.
+    // Deliverable 1: 4. Create a variable that filters the samples for the object with the desired sample number.
     let resultArray = samples.filter(sampleObj => sampleObj.id === sample);
     // console.log("result array" , resultArray);
 
-    // Deliverable 3: 1. Create a letiable that filters the metadata array for the object with the desired sample number.
+    // Deliverable 3: 1. Create a variable that filters the metadata array for the object with the desired sample number.
 
-    // Deliverable 1: 5. Create a letiable that holds the first sample in the array.
+    // Deliverable 1: 5. Create a variable that holds the first sample in the array.
     let result = resultArray[0];
-    console.log("result" , result)
+    // console.log("result" , result)
     
-    // Deliverable 3: 2. Create a letiable that holds the first sample in the metadata array.
+    // Deliverable 3: 2. Create a variable that holds the first sample in the metadata array.
 
-    // Deliverable 1: 6. Create letiables that hold the otu_ids, otu_labels, and sample_values.
+    // Deliverable 1: 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
     let otu_ids = result["otu_ids"];
     let otu_labels = result["otu_labels"];
     let sample_values = result["sample_values"];
     // console.log("sample_values" , sample_values)
 
-    // Deliverable 3: 3. Create a letiable that holds the washing frequency.
+    // Deliverable 3: 3. Create a variable that holds the washing frequency.
 
 
     // Deliverable 1: 7. Create the yticks for the bar chart.
-    // Hint: Get the the top 10 otu_ids and map them in descending order 
-    // so the otu_ids with the most bacteria are last. 
-    // Chain the slice() method with the map() and reverse() 
-    // functions to retrieve the top 10 otu_ids sorted in descending order.
-    
-    let yticks = result["sample_values"].sort().reverse().slice(0,10)
-      
-      
-    console.log("yticks" , yticks)
-    
-
+    let yticks = otu_ids.slice(0,10).reverse().map(id => `OTU ${id}`);
+    // console.log("yticks", yticks)
+     
     // Deliverable 1: 8. Create the trace for the bar chart. 
-    // let barData = [
-
-    // ];
+    let barData = [{
+      x: sample_values.slice(0,10).reverse(),
+      y: otu_ids.slice(0,10).reverse().toString(),
+      type: "bar",
+      orientation: "h",
+      // hoverinfo: otu_labels.slice(0,10).reverse(),
+    }];
 
     // Deliverable 1: 9. Create the layout for the bar chart. 
-    // let barLayout = {
-
-    // };
+    let barLayout = {
+      title: `Top Ten Bacteria Cultures Found`,
+      yaxis: {
+        ticktext: yticks
+        // showticklabels: true,
+        // ticktext: yticks.toString(),
+      }
+    };
 
     // Deliverable 1: 10. Use Plotly to plot the data with the layout. 
-
-    // Deliverable 2: 1. Create the trace for the bubble chart.
-
-    // Deliverable 2: 2. Create the layout for the bubble chart.
-
-    // Deliverable 2: 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bar", barData, barLayout);
     
+    // Deliverable 2: 1. Create the trace for the bubble chart.
+    let bubbleData = [{
+      type: "scatter",
+      mode: "markers",
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      marker: {
+        size: sample_values,
+        color: otu_ids,
+    }}];
+
+//     Using d3.select(), you can select the element that has changed and 
+// retrieve the property and HTML id that have changed.
+// Check out the Plotly bubble c
+    
+    // Deliverable 2: 2. Create the layout for the bubble chart.
+    let bubbleLayout = {
+      title: 'Bacteria Cultures Per Sample',
+      showlegend: false,
+      hovermode:'closest',
+      xaxis: {
+        title: {text: "OTU ID"},
+      }
+    };
+    // Deliverable 2: 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
     // Deliverable 3: 4. Create the trace for the gauge chart.
     
     // Deliverable 3: 5. Create the layout for the gauge chart.
